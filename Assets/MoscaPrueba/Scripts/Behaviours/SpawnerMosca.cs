985cff33973c7e9a7917c4f12b3e1dbdf21be8f7
JavierUPC <javier.ingelius@estudiantat.upc.edu>
2025-02-25 14:30:10 +0100

    Update SpawnerMosca.cs

--- Assets/MoscaPrueba/Scripts/Behaviours/SpawnerMosca.cs
+++ Assets/MoscaPrueba/Scripts/Behaviours/SpawnerMosca.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 public class SpawnerMosca : MonoBehaviour
 {
     public MoscaSO moscaSO;
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //SPAWNER
+    }
 }
