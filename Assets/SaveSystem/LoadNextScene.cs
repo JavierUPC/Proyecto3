3334377c5e687cc5b2ed0d5f060ebffa5633118a
Angl0d <javier.ingelius@estudiantat.upc.edu>
2025-06-09 18:46:05 +0200

    BlackoutScreen

--- Assets/SaveSystem/LoadNextScene.cs
+++ Assets/SaveSystem/LoadNextScene.cs
@@ -5,10 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class LoadNextScene : MonoBehaviour
 {
+    public BlackScreen blackScreen;
+
     public string sceneName;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.CompareTag("Player"))
-            SceneManager.LoadScene(sceneName);
+        {
+            blackScreen.On();
+            StartCoroutine(timeToNextLevel());
+        }
+    }
+
+    private IEnumerator timeToNextLevel()
+    {
+        yield return new WaitForSeconds(0.7f);
+        SceneManager.LoadScene(sceneName);
     }
 }
